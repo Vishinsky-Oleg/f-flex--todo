30fcc25094fa9d8051478f97b9edd1f70836fa67
Vishinky-Oleg <vishinsky.oleg@gmail.com>
2021-08-22 22:20:48 +0300

    Post build v0.1

--- src/axios-instances.ts
+++ src/axios-instances.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosJsonph = axios.create({
-    baseURL: "http://jsonplaceholder.typicode.com/",
+    baseURL: "https://jsonplaceholder.typicode.com/",
 });
 
 export const axiosFirebase = axios.create({
